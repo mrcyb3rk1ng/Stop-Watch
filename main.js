360d36a4dd36526915e802b564324c724c12952f
Hijbullah Mohammad Suhaib <160129112+mrcyb3rk1ng@users.noreply.github.com>
2024-07-03 22:53:00 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const timer = document.querySelector(".timer");
+const startBtn = document.querySelector(".startBtn");
+const stopBtn = document.querySelector(".stopBtn");
+
+function startTimer() {
+  
+  let munit = 0;
+  let second = 0;
+  
+  let interval = setInterval(() => {
+    
+    timer.textContent = munit + ":" + second;
+    container
+    if (munit < 10) {
+      timer.textContent = `0${munit}:0${second}`;
+      if (second >= 10) {
+        timer.textContent = `0${munit}:${second}`;
+      }
+    }
+    
+    second++;
+    
+    if (second == 60) {
+      munit++;
+      second = 0;
+    }
+    
+    stopBtn.addEventListener("click", () => {
+      clearInterval(interval);
+      startBtn.style.display = 'block';
+      stopBtn.style.display = 'none';
+    });
+    
+  },1000);
+}
+
+startBtn.addEventListener("click",() => {
+  startBtn.style.display = 'none';
+  stopBtn.style.display = 'block';
+  startTimer();
+});
